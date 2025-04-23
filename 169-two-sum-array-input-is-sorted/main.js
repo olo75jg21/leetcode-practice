163be048b98dfad63042876420d92be61322c92e
Jakub Olcha <jakubolcha@MacBook-Air-Jakub.local>
2025-04-23 12:10:37 +0200

    two sum array input is sorted

---
+++ 169-two-sum-array-input-is-sorted/main.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    let left = 0
+    let right = numbers.length - 1
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right]
+
+        if (sum === target) {
+            return [left + 1, right + 1]
+        }
+
+        if (sum < target) {
+            left++
+            continue
+        }
+
+        if (sum > target) {
+            right--
+            continue
+        }
+    }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9))
